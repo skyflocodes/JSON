7a6c2e54caa4cb5bbc91bc634e43b30fc942fc4d
Sky Florence <55286296+skyflocodes@users.noreply.github.com>
2020-03-25 17:36:43 -0400

    promise
    refactored the code from lab 4 using promises.

--- addison.js
+++ addison.js
@@ -1,36 +1,49 @@
-//request the json file
-let requestURL = 'https://skyflocodes.github.io/JSON/Addison.json';
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json'; 
-request.send();
+function getItems(requestURL) {
+  //request the json file
+  let request = new XMLHttpRequest();
 
-//load the json file
-request.onload = function() {
-  let addisonJson = request.response;
-  addisonItems(addisonJson); 
-};
+  return new Promise(function (resolve, reject) {
+    try {
+      request.open('GET', requestURL);
+      request.responseType = 'json';
+      request.send();
+
+      //load the json file
+      request.onload = function () {
+        let addisonJson = request.response;
+        resolve(addisonJson);
+      };
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+getItems('https://skyflocodes.github.io/JSON/Addison.json')
+  .then(function(value){
+    addisonItems(value);
+  })
 
 function addisonItems(jsonObj) {
-  
+
   //declare the addison items
-  let addisonItems = jsonObj.addisonItems; 
-  
+  let addisonItems = jsonObj.addisonItems;
+
   //for loop to print all the items in thejson file
-  for(let i = 0; i < addisonItems.length; i++) { 
+  for (let i = 0; i < addisonItems.length; i++) {
 
     //select the body of the index.html
-    let body = document.querySelector('body'); 
+    let body = document.querySelector('body');
 
     //add the name of item as header
     let h2 = document.createElement("h2");
-    h2.textContent = addisonItems[i].name; 
+    h2.textContent = addisonItems[i].name;
     body.appendChild(h2);
 
     //add the produst image
     let img = document.createElement("img");
-    img.setAttribute("src", 'https://skyflocodes.github.io/JSON/Bedcovers/' + addisonItems[i].image); 
-    img.setAttribute("alt", addisonItems[i].image); 
+    img.setAttribute("src", 'https://skyflocodes.github.io/JSON/Bedcovers/' + addisonItems[i].image);
+    img.setAttribute("alt", addisonItems[i].image);
     body.appendChild(img);
 
     //add the description and price
@@ -38,5 +51,5 @@ function addisonItems(jsonObj) {
     p.textContent = "Price: $" + addisonItems[i].price + "\n" + "Details: " + addisonItems[i].details;
     body.appendChild(p);
   }
- 
+
 }
